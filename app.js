97855a2ed5023101e1e32d2757eddb16b8e0f642
Polygonz <156954786+Polygonz2007@users.noreply.github.com>
2025-04-11 00:43:49 +0200

    More functional than before

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const config = {
 import dotenv from "dotenv";
 dotenv.config();
 
-const test_path = process.env.test_path;
+const music_path = process.env.music_path;
 
 // Path
 import path from 'path';
@@ -19,7 +19,17 @@ global.public_path = path.join(__dirname, "public");
 
 // Express
 import express from "express";
+import session from "express-session"
 const app = express();
+
+const session_parser = session({
+    secret: process.env.session_secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // If using HTTPS, set to true
+});
+
+app.use(session_parser);
 
 // HTTP
 import http from "http";
@@ -31,31 +41,139 @@ import fs from "fs";
 
 // Decoder
 import { FLACDecoder } from "@wasm-audio-decoders/flac";
+const decoder = new FLACDecoder();
 
-const parser = new codec_parser("audio/flac");
-const file = fs.readFileSync(test_path);
-const frames = parser.parseAll(file);
 
-const frame = frames[0];
-let frame_list = [];
+// WebSockets
+import WebSocket, { WebSocketServer } from 'ws';
+global.wss = new WebSocketServer({ noServer: true });
 
-for (let i = 0; i < 1000; i++) {
-    frame_list.push(frames[i]);
+server.on('upgrade', function (request, socket, head) {
+    socket.on('error', console.error);
+
+    session_parser(request, {}, () => {
+        if (false) {
+            socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+            socket.destroy();
+            return;
+        }
+    
+        socket.removeListener('error', console.error);
+    
+        wss.handleUpgrade(request, socket, head, function (ws) {
+            wss.emit('connection', ws, request);
+        });
+    });
+});
+
+wss.on('connection', (ws, req) => {
+
+    ws.on('message', async (data, isBinary) => {
+        // Translate
+        data = isBinary ? data : data.toString();
+        data = JSON.parse(data);
+
+        const client = req.session;
+
+        console.log(data)
+
+        // Functions
+        const type = data.type;
+        switch (type) {
+            case "play":
+                client.song = data.song;
+
+                const file_path = `${music_path}/${client.song}.flac`;
+                console.log('Loading "' + file_path + "'");
+
+                const parser = new codec_parser("audio/flac");
+                const file = fs.readFileSync(file_path);
+                const frames = parser.parseAll(file);
+                client.frames = frames;
+
+                client.frame_index = 0;
+                client.buffer_size = 16; // Move 16 frames per call (2s)
+
+                client.frame_size = frames[0].samples;
+                console.log(frames[0])
+
+                return ws.send(prepare_json(2, {
+                    "sampleRate": frames[0].header.sampleRate,
+                    "totalLength": 44100,
+
+                    "bufferLength": client.frame_size * client.buffer_size
+
+                    // send various metadata and other stuff
+                }));
+
+            case "next_buf":
+                const size = client.frame_size * client.buffer_size * 4; // 4 bytes per 32 bit sample
+                let sample_data = [];
+
+                let temp_frames = [];
+
+                console.log("Frame indx " + client.frame_index)
+
+                const frame_offset = client.frame_index * client.buffer_size;
+                for (let i = 0; i < client.buffer_size; i++) {
+                    const frame_number = frame_offset + i;
+                    //console.log(`Fetching frame ${frame_number}`)
+
+                    //if (frame_number > client.frames.length)
+                    //    continue;
+
+                    const frame = client.frames[frame_number];
+                    temp_frames.push(frame);
+                //    console.log(frame);
+                //
+                //    for (let channel = 0; channel < 2; channel++) {
+                //        sample_data[channel] = decoder.decodeFrames(frame);//new Int32Array(size);
+                //        //for (const sample of frame.channelData) { sample_data[channel].push(sample); }
+                //    }
+                }
+
+                //for (let channel = 0; channel < 2; channel++) {
+                if (temp_frames.length == 0)
+                    ws.send("Done")
+
+                const decoded = await decoder.decodeFrames(temp_frames);
+                //}
+
+                // Create buffer with message type and buffer index and audio data
+                const metadata_length = 8;
+                const channel_length = size * 2; // 4 bytes per sample, 2 channels
+                const buffer = Buffer.alloc(metadata_length + channel_length);
+
+                buffer.writeUInt32LE(1, 0);
+                buffer.writeUInt32LE(client.frame_index, 4);
+
+                Buffer.from(decoded.channelData[0].buffer).copy(buffer, 8);
+                Buffer.from(decoded.channelData[1].buffer).copy(buffer, 8 + size);
+
+                // Send
+                client.frame_index++;
+                return ws.send(buffer);
+        }
+    });
+
+    ws.on('close', () => {
+        // Handle connection close
+    });
+});
+
+
+// move to differtent plaves idiot
+// Converts JSON into a Buffer with type, along with JSON data for client
+function prepare_json(type, data) {
+    const stringed = JSON.stringify(data);
+    const buf = Buffer.alloc(4 + stringed.length);
+
+    buf.writeUInt32LE(type);
+    Buffer.from(stringed).copy(buf, 4);
+
+    return buf;
 }
 
-console.log(frame)
-
-app.get("/test", async (req, res) => {
-    const decoder = new FLACDecoder();
-    await decoder.ready;
-
-    const data = await decoder.decodeFrames(frame_list);
-    console.log(data)
-
-
-    console.log(data);
-    res.json(data);
-})
 
 // Start server
 app.use(express.static(global.public_path));
--- public/script.js
+++ public/script.js
@@ -1,13 +1,99 @@
 
 const doc = document;
-const play_btn = document.querySelector("#play");
+const play_btn = doc.querySelector("#play");
+const song_field = doc.querySelector("#song");
+
+const audioCtx = new AudioContext();
 
 const player = {
-  volume: 0.5 // -3db
+  volume: 0.5, // -3db
+
+  next_start_time: 0
 }
 
 
 let audio_buffer = [];
+let active_buffer = 0;
+
+
+// Websocket
+const url = `ws://${window.location.host}`;
+const socket = new WebSocket(url);
+
+socket.addEventListener("open", () => {
+  // Yay
+});
+
+play_btn.addEventListener("click", () => {
+  ws_request("play", {"song": song.value});
+  ws_request("next_buf");
+  ws_request("next_buf");
+})
+
+socket.addEventListener("message", async (event) => {
+  const data = await event.data.arrayBuffer();
+  console.log(data)
+  
+  //switch d
+  const data_view = new DataView(data)
+  
+  const type = data_view.getUint32(0, true);
+
+  switch (type) {
+    case 1: // Buffer data
+      // Check order
+      const buffer_index = data_view.getUint32(4, true);
+      console.log(`Loading buffer #${buffer_index}`);
+
+      // Create audio buffer
+      console.log(data.byteLength)
+      const per_channel = ((data.byteLength / 4) - 8) / 2;
+      console.log("P?er channel " + per_channel)
+      
+      let channel_data = [
+        new Float32Array(per_channel), //data.splice(per_channel)),
+        new Float32Array(per_channel)  //data.splice(per_channel))
+      ];
+
+      for (let i = 0; i < per_channel; i++) {
+        channel_data[0][i] = data_view.getFloat32(4 * i + 8, true);
+        channel_data[1][i] = data_view.getFloat32(4 * (i + per_channel) + 8, true);
+      }
+
+      active_buffer = 1 - active_buffer;
+      if (buffer_index == 0)
+        player.next_start_time = audioCtx.currentTime;
+      
+      audio_buffer[active_buffer] = prepare_source(channel_data, per_channel, player.sampleRate);
+      audio_buffer[active_buffer].source.start(player.next_start_time);
+      player.next_start_time += audio_buffer[active_buffer].buffer.duration;
+
+      audio_buffer[1 - active_buffer].source.addEventListener('ended', () => {
+        ws_request("next_buf");
+      });
+
+      if (buffer_index == 0)
+        active_buffer = 1 - active_buffer;
+      
+      return;
+
+
+    case 2: // Start song
+      const info = get_json(data);
+      player.sampleRate = info.sampleRate;
+      player.totalLength = info.totalLength;
+      player.bufferLength = info.bufferLength;
+
+      // Start playback
+      player.bufferLengthMS = (player.bufferLength / player.sampleRate) * 1000;
+
+      return;
+  }
+
+  console.log(raw)
+
+  //console.log(await yes.bytes())
+});
 
 (async () => {
   // Flac decoder
@@ -30,89 +116,69 @@ let audio_buffer = [];
     prepare_source(decoded.channelData, decoded.samplesDecoded, decoded.samplerate);
   }
 
-  // Audio Context
-  const audioCtx = new AudioContext();
-
-  function prepare_source(data, length, samplerate) {
-    const buffer = audioCtx.createBuffer(2, length, samplerate);
-
-    // Fill the buffer with the data
-    for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
-      const nowBuffering = buffer.getChannelData(channel);
-      console.log(data[channel])
-      //nowBuffering = data[channel];
-
-      //const interval = Math.floor(length / 20);
+  // start the source playing
+  //play_btn.addEventListener("click", () => {
+  //  const current = audio_buffer[0];
+  //  const source = prepare_source(current.data, current.samples, current.rate);
 //
-      for (let i = 0; i < length; i++) {
-        nowBuffering[i] = data[channel][i] * player.volume;
-      }
+  //  source.start();
+  //})
+})()
+
+
+
+// Audio Context
+//const audioCtx = new AudioContext();
+
+function prepare_source(data, length, samplerate) {
+  const buffer = audioCtx.createBuffer(2, length, samplerate);
+
+  // Fill the buffer with the data
+  for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
+    const nowBuffering = buffer.getChannelData(channel);
+    console.log(data[channel])
+    //nowBuffering = data[channel];
+
+    //const interval = Math.floor(length / 20);
+//
+    for (let i = 0; i < length; i++) {
+      nowBuffering[i] = data[channel][i] * player.volume;
     }
-
-    // Get an AudioBufferSourceNode
-    const source = audioCtx.createBufferSource();
-    source.buffer = buffer;
-    source.connect(audioCtx.destination);
-
-    console.log("Starting playback")
-
-    return source;
   }
 
-  // start the source playing
-  play_btn.addEventListener("click", () => {
-    const current = audio_buffer[0];
-    const source = prepare_source(current.data, current.samples, current.rate);
+  // Get an AudioBufferSourceNode
+  const source = audioCtx.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioCtx.destination);
 
-    source.start();
-  })
+  console.log("Starting playback")
+
+  return {buffer: buffer, source: source};
+}
 
 
-  console.log("Getting test frame");
+// Websocket helping
+async function ws_request(type, data) {
+  if (!data)
+    data = {};
 
-  // Get and play frame
-  //try {
-      const resp = await fetch("test");
-      console.log(resp)
-      const data = await resp.json();
-      console.log(data)
+  data.type = type;
+  socket.send(JSON.stringify(data));
+}
 
-      // Get data
-      audio_buffer[0] = {};
 
-      const num_samples = data.samplesDecoded;
-      audio_buffer[0].samples = num_samples;
-      audio_buffer[0].rate = data.sampleRate;
 
-      audio_buffer[0].data = [];
-      audio_buffer[0].data[0] = new Float32Array(num_samples);
-      audio_buffer[0].data[1] = new Float32Array(num_samples);
 
-      for (let i = 0; i < num_samples; i++) {
-        if (i == 10000)
-          console.log(data.channelData[0][i]);
 
-        audio_buffer[0].data[0][i] = data.channelData[0][i];
-        audio_buffer[0].data[1][i] = data.channelData[1][i];
-      }
 
-      //audio_buffer[0].data = data.channelData;
 
-      console.log(audio_buffer[0]);
+// Maybe gbet from somehwerew else
+function get_json(data) {
+  let buf = new Uint8Array(data);
+  buf = buf.slice(4)
 
-      play_btn.removeAttribute("disabled");
-
-      // Play
-      const current = audio_buffer[0];
-      const source = prepare_source(current.data, current.samples, current.rate);
-      source.start();
-
-      // Handle response
-      //decode_frame(data)
-      console.log("decoded")
-  //} catch {
-  //    console.log("Couldnt do that yikes");
-  //}
-
-  console.log("Fin")
-})()
+  console.log(buf.toString())
+  const string = String.fromCharCode.apply(null, buf);
+  console.log(string)
+  return JSON.parse(string);
+}
